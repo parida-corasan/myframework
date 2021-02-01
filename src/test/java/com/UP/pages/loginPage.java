be766a8ae7ecdc88bff8360cbc77f51618517512
parida corasan <64044358+parida-corasan@users.noreply.github.com>
2021-01-31 22:35:47 -0500

    Update loginPage.java
    add some comment......not important

--- src/test/java/com/UP/pages/loginPage.java
+++ src/test/java/com/UP/pages/loginPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class loginPage {
-
+// Upgenix login page
     public loginPage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
