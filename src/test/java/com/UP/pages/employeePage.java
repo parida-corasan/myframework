d32679ba2b74a101719bfb47ef036a601ff0490e
Parida Abubekir <peride.ab@gmail.com>
2021-01-31 23:18:46 -0500

    parida branch

--- src/test/java/com/UP/pages/employeePage.java
+++ src/test/java/com/UP/pages/employeePage.java
@@ -1,4 +1,11 @@
-package com.UP.pages;public class employeePage {
+package com.UP.pages;
+
+import com.UP.Utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class employeePage {
   
   public employeePage(){
         PageFactory.initElements(Driver.getDriver(),this);
@@ -12,7 +19,7 @@ package com.UP.pages;public class employeePage {
     @FindBy()
     public WebElement  sth2;
 
-
+// from my branch
     
   
   
