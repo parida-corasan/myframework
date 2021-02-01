e4e68649b6aab40c0743742f201ae8430b3a6696
parida corasan <64044358+parida-corasan@users.noreply.github.com>
2021-01-31 22:59:54 -0500

    Update employeePage.java
    and WebElement to Employee page

--- src/test/java/com/UP/pages/employeePage.java
+++ src/test/java/com/UP/pages/employeePage.java
@@ -1,2 +1,18 @@
 package com.UP.pages;public class employeePage {
+  
+  public employeePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "")
+  public WebElement sth1;
+
+
+
+    @FindBy()
+    public WebElement  sth2;
+
+
+    @FindBy()
+    public WebElement sth3;
 }
